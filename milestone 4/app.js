6f59f4004f5736b893103ee95f51664985291627
Zoha Nadeem <160650478+ZohaScripter@users.noreply.github.com>
2024-11-10 21:03:20 +0500

    Add files via upload

---
+++ milestone 4/app.js
@@ -0,0 +1,20 @@
+var form = document.getElementById('form');
+var display = document.getElementById('generate-resume');
+form.addEventListener('submit', function (event) {
+    event.preventDefault();
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var contact = document.getElementById('contact').value;
+    var education = document.getElementById('education').value;
+    var experience = document.getElementById('experience').value;
+    var skills = document.getElementById('skills').value;
+    // generating the resume content dynamically
+    var htmlResume = "\n    <h2><b>Editable Resume</b></h2>\n    <h3>Personal Information</h3>\n    <p><b>Name:</b><span contenteditable=\"true\">".concat(name, "</span></p>\n    <p><b>Email:</b><span contenteditable=\"true\">").concat(email, "</span></p>\n    <p><b>Contact:</b><span contenteditable=\"true\">").concat(contact, "</span></p>\n\n    <h3>Education</h3>\n    <p contenteditable=\"true\">").concat(education, "</p>\n\n    <h3>Experience</h3>\n    <p contenteditable=\"true\">").concat(experience, "</p>\n\n    <h3>Skills</h3>\n    <p contenteditable=\"true\">").concat(skills, "</p>\n    ");
+    // displaying our generated resume
+    if (display) {
+        display.innerHTML = htmlResume;
+    }
+    else {
+        console.error('display element is missing');
+    }
+});
